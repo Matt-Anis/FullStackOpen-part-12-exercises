7397c1f2c47a858205b1cfe38fc25b6500e47e0c
Matt Anis <webdev.omis@gmail.com>
2026-07-21 23:51:59 +0100

    21. The smoke test
    (did not change any text just prettier doing its thing)

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const redis = require("../redis");
 
 router.get("/", async (_, res) => {
-  const added_todos = (await redis.get("todos_count")) || 0;
-  const visits = (await redis.get("visits")) || 0;
+  const added_todos = Number((await redis.get("todos_count")) || 0);
+  const visits = Number((await redis.get("visits")) || 0);
 
   res.json({ added_todos, visits });
 });
